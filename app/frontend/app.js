b42589190abc659c9a8f9e6a1549dc725bf1cdc9
Remmey <markremmey@microsoft.com>
2023-07-24 15:07:19 -0400

    Updating with vite build

--- app/frontend/app.js
+++ app/frontend/app.js
@@ -1,54 +1,22 @@
-import React, { useEffect, useState } from "react";
-import axios from "axios";
-import { DragDropContext, Draggable, Droppable } from "react-beautiful-dnd";
+import React from 'react';
+import Item from './Item';
+import './style.css';
+import axios from 'axios';
 
 function App() {
-  const [transactions, setTransactions] = useState([]);
-
-  useEffect(() => {
-    async function fetchData() {
-      const result = await axios.get("http://localhost:5000/get_record");
-      setTransactions(result.data);
-    }
-
-    fetchData();
-  }, []);
-
-  function handleDragEnd(result) {
-    if (!result.destination) return;
-
-    const newTransactions = Array.from(transactions);
-    const [movedTransaction] = newTransactions.splice(result.source.index, 1);
-    newTransactions.splice(result.destination.index, 0, movedTransaction);
-
-    setTransactions(newTransactions);
-    // Here, you might make another API call to update the data in your backend.
-  }
+  const items = ['Apple', 'Banana', 'Cherry', 'Canelope'];
+  const response = axios.get('get-transaction.azurewebsites.net/get_record');
 
   return (
-    <DragDropContext onDragEnd={handleDragEnd}>
-      <Droppable droppableId="droppable">
-        {(provided) => (
-          <div ref={provided.innerRef} {...provided.droppableProps}>
-            {transactions.map((transaction, index) => (
-              <Draggable key={transaction.id} draggableId={transaction.id} index={index}>
-                {(provided) => (
-                  <div
-                    ref={provided.innerRef}
-                    {...provided.draggableProps}
-                    {...provided.dragHandleProps}
-                  >
-                    {/* Your transaction display goes here */}
-                    <span>{transaction.name}</span>
-                  </div>
-                )}
-              </Draggable>
-            ))}
-            {provided.placeholder}
-          </div>
-        )}
-      </Droppable>
-    </DragDropContext>
+    <div className="App">
+      <div className="centered">
+        <ul>
+          {items.map((item, index) => (
+            <Item key={index} name={item} />
+          ))}
+        </ul>
+      </div>
+    </div>
   );
 }
 
